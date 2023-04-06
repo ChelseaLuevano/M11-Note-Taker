aaf670841d8d19fc638526ce88b925c1a48ff272
Chelsea-Luevano <120981491+ChelseaLuevano@users.noreply.github.com>
2023-04-06 07:41:49 -0400

    created server.js file and added some beginning code needed in file, such as defining app, port, and requiring express library

---
+++ server.js
@@ -0,0 +1,17 @@
+// import express
+const express = require('express');
+
+// Initialize an app object/instance of express
+const app = express();
+
+// PORT variable
+const PORT = 3001;
+
+// Function for app port
+app.listen(PORT, () => {
+// determine what code to in this
+
+})
+
+
+// define routes for appliction with path & callback function 
